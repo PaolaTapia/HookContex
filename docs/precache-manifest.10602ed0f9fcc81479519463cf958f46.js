1af816a9303b9f4b3dd9c8ed57a2398a47bee771
Paola Tapia <email@gmail.com>
2020-09-22 20:44:12 -0300

    first commit

---
+++ docs/precache-manifest.10602ed0f9fcc81479519463cf958f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750560efcb0ed978c0f7b847f9d84897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9cfa429b24fa8d2384",
+    "url": "/static/js/2.678f1ff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.678f1ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d98ae6a30efdfe87fce",
+    "url": "/static/js/main.810a2046.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
